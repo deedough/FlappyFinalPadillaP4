6b2d8f0807acb6eaeecc76a9b1894911d5782ce1
deedough <nicholasomarpadilla@ca.rr.com>
2022-12-20 12:41:03 -0800

    Added Everything the Game is FINISHED

--- FlappyBirdFinalP4Padilla/Assets/Scripts/Bird.cs
+++ FlappyBirdFinalP4Padilla/Assets/Scripts/Bird.cs
@@ -31,6 +31,7 @@ public class Bird : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
+        rb2d.velocity = Vector2.zero;
         isDead = true;
         anim.SetTrigger("Die");
         GameControl.instance.BirdDied();
--- FlappyBirdFinalP4Padilla/Assets/Scripts/GameControl.cs
+++ FlappyBirdFinalP4Padilla/Assets/Scripts/GameControl.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEditorInternal;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOvertext;
+    public TMP_Text scoreText;
     public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+    private int score = 0;
    
     void Awake()
     {
@@ -22,6 +30,16 @@ public class GameControl : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+    }
+
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score:" + score.ToString ();
     }
 
     public void BirdDied()
